bcd59320f65d81e61df2b894708253b508a12dc4
Bryce Kerley <bkerley@brycekerley.net>
2012-10-11 15:15:48 -0700

    initial commit

---
+++ lib/riak-dt-ruby.rb
@@ -0,0 +1,9 @@
+require "riak-dt-ruby/version"
+
+module Riak
+  module Dt
+    module Ruby
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/riak-dt-ruby/version.rb
@@ -0,0 +1,7 @@
+module Riak
+  module Dt
+    module Ruby
+      VERSION = "0.0.1"
+    end
+  end
+end
