33d7c7f9b45c0af68580fd68e4e5c3e19e744d7e
Bryce Kerley <bkerley@brycekerley.net>
2012-10-11 15:38:20 -0700

    load dependencies

--- lib/riak-dt-ruby.rb
+++ lib/riak-dt-ruby.rb
@@ -1,9 +1,15 @@
-require "riak-dt-ruby/version"
+%w{faraday cgi json}.each do |f|
+  require f
+end
+%w{version set counter}.each do |f|
+  require_relative "riak-dt-ruby/#{f}"
+end
 
-module Riak
-  module Dt
-    module Ruby
-      # Your code goes here...
+
+module RiakDt
+  def self.client
+    @client ||= Faraday.new url: 'http://localhost:8091/' do |builder|
+      builder.adapter :net_http
     end
   end
 end
